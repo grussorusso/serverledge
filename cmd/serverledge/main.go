c8aaf4d615a01acf3b4aad2778a47e06bdaabeca
Gabriele Russo Russo <gabri.russo17@gmail.com>
2021-10-19 17:17:23 +0200

    Adds Request and Schedule()

--- cmd/serverledge/main.go
+++ cmd/serverledge/main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/grussorusso/serverledge/pkg/faas"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
-	// Hello world, the web server
+func getFunctions(c echo.Context) error {
+	return c.JSON(http.StatusOK, "No functions in the system.")
+}
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+func invokeFunction(c echo.Context) error {
+	funcName := c.Param("fun")
+	log.Printf("New request for function `%s`", funcName)
+
+	r := &faas.Request{funcName, time.Now()}
+	if err := faas.Schedule(r); err == nil {
+		return c.JSON(http.StatusOK, "OK")
+	} else {
+		return c.JSON(http.StatusOK, "Failed")
 	}
+}
 
-	http.HandleFunc("/hello", helloHandler)
-    log.Println("Listing for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+func startAPIServer() {
+	e := echo.New()
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.POST("/invoke/:fun", invokeFunction)
+	//e.GET("/users/:id", getUser)
+	e.GET("/functions", getFunctions)
+
+	// Start server
+	e.HideBanner = true
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func main() {
+	startAPIServer()
 }
