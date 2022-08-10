8b136d945223ad889d8314a4ac421643191c5bf3
Gabriele Russo Russo <gabri.russo17@gmail.com>
2022-08-10 17:07:46 +0200

    Custom registry

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -4,16 +4,15 @@ import (
 	"log"
 
 	"net/http"
-	"time"
 
 	"github.com/grussorusso/serverledge/internal/config"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var Enabled bool
+var registry = prometheus.NewRegistry()
 
 func Init() {
 	if config.GetBool(config.METRICS_ENABLED, false) {
@@ -25,24 +24,26 @@ func Init() {
 		return
 	}
 
-	recordMetrics()
-
-	http.Handle("/metrics", promhttp.Handler())
+	handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{
+		EnableOpenMetrics: true})
+	http.Handle("/metrics", handler)
 	http.ListenAndServe(":2112", nil)
 }
 
-func recordMetrics() {
-	go func() {
-		for {
-			opsProcessed.Inc()
-			time.Sleep(2 * time.Second)
-		}
-	}()
-}
-
-var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myapp_processed_ops_total",
-		Help: "The total number of processed events",
-	})
-)
+// Example
+//var (
+//	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+//		Name: "myapp_processed_ops_total",
+//		Help: "The total number of processed events",
+//	})
+//)
+//
+//func recordMetrics() {
+//	registry.Register(opsProcessed)
+//	go func() {
+//		for {
+//			opsProcessed.Inc()
+//			time.Sleep(2 * time.Second)
+//		}
+//	}()
+//}
