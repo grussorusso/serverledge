68d2bb662587b37fd4944a6f0018699051345f75
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2022-12-06 11:25:26 +0100

    Edited create-functions.sh

--- scripts/create-functions.sh
+++ scripts/create-functions.sh
@@ -1,2 +1,3 @@
-bin/serverledge-cli create -f func --memory 600 --src examples/hello.py --runtime python310 --handler "hello.handler" -H 192.168.1.13
-bin/serverledge-cli create -f fib --memory 600 --src examples/fibonacci.py --runtime python310 --handler "fibonacci.handler" -H 192.168.1.13
+#!/bin/sh
+bin/serverledge-cli create -f func --memory 600 --src examples/hello.py --runtime python310 --handler "hello.handler" -H "$1"
+bin/serverledge-cli create -f fib --memory 600 --src examples/fibonacci.py --runtime python310 --handler "fibonacci.handler" -H "$1"
