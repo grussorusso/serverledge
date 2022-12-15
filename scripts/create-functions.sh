c7db2c183f09221ba01afc508de4a80864612866
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2022-12-15 11:03:24 +0100

    Added fibonacci_nout.py for testing purposes

--- scripts/create-functions.sh
+++ scripts/create-functions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 bin/serverledge-cli create -f func --memory "$2" --src examples/hello.py --runtime python310 --handler "hello.handler" -H "$1"
-bin/serverledge-cli create -f fib --memory "$2" --src examples/fibonacci_nout.py --runtime python310 --handler "fibonacci_nout.handler" -H "$1"
+bin/serverledge-cli create -f fib --memory "$2" --src examples/fibonacci_nout.py --runtime python310 --handler "fibonacci.handler" -H "$1"
