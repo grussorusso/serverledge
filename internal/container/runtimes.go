112b1e76c233d2a9fdd85c51cf6ad46789841b74
IronMatt97 <matti1097@gmail.com>
2022-12-20 11:06:54 +0100

    Bug fix: runtimes.go containerManager acquisition which was different from the scheduler

--- internal/container/runtimes.go
+++ internal/container/runtimes.go
@@ -1,5 +1,7 @@
 package container
 
+import "github.com/grussorusso/serverledge/internal/config"
+
 //RuntimeInfo contains information about a supported function runtime env.
 type RuntimeInfo struct {
 	Image         string
@@ -10,9 +12,19 @@ const CUSTOM_RUNTIME = "custom"
 
 var refreshedImages = map[string]bool{}
 
+var RuntimeToInfo = getRuntimeInfo()
+
 // Podman requires the prefix 'docker.io' in order to pull from DockerHub
-var RuntimeToInfo = map[string]RuntimeInfo{
-	"python310":  {"docker.io/grussorusso/serverledge-python310", []string{"python", "/entrypoint.py"}},
-	"nodejs17":   {"docker.io/grussorusso/serverledge-nodejs17", []string{"node", "/entrypoint.js"}},
-	"nodejs17ng": {"docker.io/grussorusso/serverledge-nodejs17ng", []string{}},
+func getRuntimeInfo() map[string]RuntimeInfo {
+	config.ReadConfiguration(config.DefaultConfigFileName)
+	containerManager := config.GetString(config.DEFAULT_CONTAINER_MANAGER, "podman")
+	prefix := ""
+	if containerManager == "podman" {
+		prefix = "docker.io/"
+	}
+	return map[string]RuntimeInfo{
+		"python310":  {prefix + "grussorusso/serverledge-python310", []string{"python", "/entrypoint.py"}},
+		"nodejs17":   {prefix + "grussorusso/serverledge-nodejs17", []string{"node", "/entrypoint.js"}},
+		"nodejs17ng": {prefix + "grussorusso/serverledge-nodejs17ng", []string{}},
+	}
 }
