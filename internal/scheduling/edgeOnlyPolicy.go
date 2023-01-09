eb6ec23a6cd354385c1e95d692c61206dc115104
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2023-01-09 14:37:46 +0100

    Update Edge Only policy

--- internal/scheduling/edgeOnlyPolicy.go
+++ internal/scheduling/edgeOnlyPolicy.go
@@ -17,20 +17,18 @@ func (p *EdgePolicy) OnCompletion(r *scheduledRequest) {
 }
 
 func (p *EdgePolicy) OnArrival(r *scheduledRequest) {
-	if r.CanDoOffloading {
+	containerID, err := node.AcquireWarmContainer(r.Fun)
+	if err == nil {
+		log.Printf("Using a warm container for: %v", r)
+		execLocally(r, containerID, true)
+	} else if r.CanDoOffloading {
 		url := pickEdgeNodeForOffloading(r)
 		if url != "" {
 			handleEdgeOffload(r, url)
 			return
 		}
-	} else {
-		containerID, err := node.AcquireWarmContainer(r.Fun)
-		if err == nil {
-			log.Printf("Using a warm container for: %v", r)
-			execLocally(r, containerID, true)
-		} else if handleColdStart(r) {
-			return
-		}
+	} else if handleColdStart(r) {
+		return
 	}
 
 	dropRequest(r)
