277a71c919c67d4657ad0b1be1feb249b2813229
Daniele Ferrarelli <danieleferrarelli@gmail.com>
2023-01-26 11:50:29 +0100

    Added custom timeout for etcd

--- utils/etcd.go
+++ utils/etcd.go
@@ -22,9 +22,10 @@ func GetEtcdClient() (*clientv3.Client, error) {
 	}
 
 	etcdHost := config.GetString(config.ETCD_ADDRESS, "localhost:2379")
+	timeout := config.GetInt(config.ETCD_TIMEOUT, 1)
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   []string{etcdHost},
-		DialTimeout: 1 * time.Second,
+		DialTimeout: time.Duration(timeout) * time.Second,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("Could not connect to etcd: %v", err)
