d3dc1b4a8806e49535fbbe9c8eb7c74bf048a0a6
giacomo <giacomo.redjack@gmail.com>
2023-09-28 19:56:34 +0200

    etcd now uses only 1 core

--- scripts/start-etcd.sh
+++ scripts/start-etcd.sh
@@ -2,6 +2,7 @@
 docker run -d --rm --name Etcd-server \
     --publish 2379:2379 \
     --publish 2380:2380 \
+    --cpus="1" \
     --env ALLOW_NONE_AUTHENTICATION=yes \
     --env ETCD_ADVERTISE_CLIENT_URLS=http://localhost:2379 \
     bitnami/etcd:latest
