87741ae1ae4c0b0d132d94eeb133f90b049ee4f1
giacomo <giacomo.redjack@gmail.com>
2024-04-25 18:39:24 +0200

    Added code from asl_parser

---
+++ internal/fc/asl_parser.go
@@ -0,0 +1,148 @@
+/*** Based on https://github.com/enginyoyen/aslparser ***/
+package fc
+
+/*
+import (
+	"encoding/json"
+	"github.com/xeipuuv/gojsonschema"
+	"io/ioutil"
+)
+
+type Retry struct {
+	ErrorEquals     []string
+	IntervalSeconds int
+	BackoffRate     int
+	MaxAttempts     int
+}
+
+type Catch struct {
+	ErrorEquals []string
+	ResultPath  string
+	Next        string
+}
+
+type State struct {
+	Comment          string
+	Type             string
+	Next             string
+	Default          string
+	Resource         string
+	End              bool
+	Parameters       map[string]interface{}
+	Retry            []Retry
+	Catch            []Catch
+	TimeoutSeconds   int
+	HeartbeatSeconds int
+}
+
+type StateMachine struct {
+	Comment          string
+	StartAt          string
+	Version          string
+	States           map[string]State
+	validationResult *gojsonschema.Result
+}
+
+// Given the file path validates and returns the StateMachine
+// strict argument defines whether Resource name must be AWS ARN pattern or not
+func ParseFile(filepath string, strict bool) (*StateMachine, error) {
+	//load file
+	payload, fileErr := ioutil.ReadFile(filepath)
+	if fileErr != nil {
+		return nil, fileErr
+	}
+	return Parse(payload, strict)
+}
+
+// Given the file content validates and returns the StateMachine
+// strict argument defines whether Resource name must be AWS ARN pattern or not
+func Parse(content []byte, strict bool) (*StateMachine, error) {
+
+	// validate it, if there is an error or document is not Valid
+	// return the result without further analysis
+	var stateMachine StateMachine
+	validationResult, valErr := Validate(content, strict)
+	stateMachine.validationResult = validationResult
+	if valErr != nil || !validationResult.Valid() {
+		return &stateMachine, valErr
+	}
+
+	// given state-machine payload is valid, unmarshal the json file
+	unmarshalErr := json.Unmarshal(content, &stateMachine)
+	if unmarshalErr != nil {
+		return &stateMachine, unmarshalErr
+	}
+	// find and register non-semantic errors
+	stateMachine.findAndRegisterSchemaErrors()
+	return &stateMachine, nil
+}
+
+// Valid returns true if no errors were found
+func (s *StateMachine) Valid() bool {
+	return s.validationResult.Valid()
+}
+
+// Errors returns the errors that were found
+func (s *StateMachine) Errors() []gojsonschema.ResultError {
+	return s.validationResult.Errors()
+}
+
+func (s *StateMachine) findAndRegisterSchemaErrors() {
+	missingStates := *s.findNonSchemaErrors()
+
+	for k, v := range missingStates {
+		err := new(gojsonschema.MissingDependencyError)
+		err.SetType("missing_dependency")
+		err.SetDescriptionFormat(gojsonschema.Locale.MissingDependency())
+		details := gojsonschema.ErrorDetails{
+			"dependency": k + ". " + v,
+		}
+		err.SetDetails(details)
+		s.validationResult.AddError(err, details)
+	}
+}
+
+func (s *StateMachine) findNonSchemaErrors() *map[string]string {
+	var errors = make(map[string]string)
+
+	if !s.statePresent(s.StartAt) {
+		errors[s.StartAt] = "Missing 'StartAt' transition target. Could not locate " + s.StartAt
+	}
+
+	for k, v := range s.States {
+		if !s.targetStateRegistered(k) {
+			errors[k] = k + " is defined but is not reachable."
+		}
+		if len(v.Next) > 0 && !s.statePresent(v.Next) {
+			errors[k] = v.Next + " as Next,defined in " + k + ", but not reachable"
+		}
+		if len(v.Default) > 0 && !s.statePresent(v.Default) {
+			errors[k] = v.Default + " as Default, defined in " + k + ", but not reachable"
+		}
+	}
+	return &errors
+}
+
+func (s *StateMachine) statePresent(state string) bool {
+	_, present := s.States[state]
+	return present
+}
+func (s *StateMachine) targetStateRegistered(state string) bool {
+	if s.StartAt == state {
+		return true
+	}
+
+	match := false
+	for _, v := range s.States {
+		if len(v.Next) > 0 && v.Next == state {
+			match = true
+			break
+		}
+
+		if len(v.Default) > 0 && v.Next == state {
+			match = true
+			break
+		}
+	}
+	return match
+}*/
---
+++ internal/fc/asl_validator.go
@@ -0,0 +1,39 @@
+/*** Based on https://github.com/enginyoyen/aslparser ***/
+package fc
+
+import (
+	"github.com/xeipuuv/gojsonschema"
+)
+
+// Loads the state-machine JSON file from provided path
+// and validates it against states-language schema
+// strict argument defines whether Resource name must be AWS ARN pattern or not
+// See https://states-language.net/spec.html
+func Validate(payload []byte, strict bool) (*gojsonschema.Result, error) {
+	result, err := validateSchema(payload, strict)
+	if err != nil {
+		return result, err
+	}
+	return result, nil
+}
+
+func validateSchema(payload []byte, strict bool) (*gojsonschema.Result, error) {
+	stateMachineSchema, assetError := stateMachineSchema(strict)
+	if assetError != nil {
+		return nil, assetError
+	}
+	schemaLoader := gojsonschema.NewStringLoader(string(stateMachineSchema))
+	documentLoader := gojsonschema.NewStringLoader(string(payload))
+
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	return result, err
+}
+
+func stateMachineSchema(strict bool) ([]byte, error) {
+	if strict {
+		return static.Asset("schemas/state-machine-strict-arn.json")
+	} else {
+		return static.Asset("schemas/state-machine.json")
+	}
+
+}
